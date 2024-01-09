09d2c0d19d0eb1e956bb6e359f85a78f26e51c72
Rndmun <91401175+Rndmun@users.noreply.github.com>
2024-01-08 22:22:54 -0800

    reflection 09 -acessing and displaying methods of the Simple class using Java's reflection API

---
+++ reflection_api/Reflection09.java
@@ -0,0 +1,28 @@
+package reflection_api;
+
+import reflection_api.Simple;
+import java.lang.reflect.Method;
+
+/**
+ * This experiment demonstrates accessing and displaying methods of the Simple class
+ * using Java's reflection API. It shows that reflection allows retrieval of method
+ * names and parameter types, including both public and inherited methods.
+ */
+public class Reflection09 {
+    public static void main(String[] args) throws Exception {
+        Simple s = new Simple();
+        Method[] methods = s.getClass().getDeclaredMethods();
+        System.out.printf("There are %d methods\n", methods.length);
+
+        for (Method m : methods) {
+            m.setAccessible(true); // Allowing access to private methods
+            System.out.printf("method name=%s type=%s parameters = ", m.getName(),
+                    m.getReturnType().getSimpleName());
+            Class<?>[] types = m.getParameterTypes();
+            for (Class<?> c : types) {
+                System.out.print(c.getName() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
